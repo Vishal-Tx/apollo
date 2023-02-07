c39a2a22d8124d03d947c2d90212cbaed28137d5
vishal-Tx <vishalguptaa2z111@gmail.com>
2023-02-07 18:44:31 +0530

    apollo client added

--- apollo/src/App.jsx
+++ apollo/src/App.jsx
@@ -1,15 +1,30 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
 import { Home, Info } from "./components";
 
 const App = () => {
+  const client = new ApolloClient({
+    uri: "https://countries.trevorblades.com",
+    cache: new InMemoryCache(),
+  });
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" exact element={<Home />} />
-        <Route path="/search" exact element={<Info />} />
-      </Routes>
-    </Router>
+    <>
+      <ApolloProvider client={client}>
+        <Router>
+          <Routes>
+            <Route path="/" exact element={<Home />} />
+            <Route path="/search" exact element={<Info />} />
+          </Routes>
+        </Router>
+      </ApolloProvider>
+    </>
   );
 };
 
