517283eb810a83bbc4868e87ac988d848a07eb5c
vishal-Tx <vishalguptaa2z111@gmail.com>
2023-02-07 18:18:15 +0530

    routes added

--- apollo/src/App.jsx
+++ apollo/src/App.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home, Info } from "./components";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/search" exact element={<Info />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
--- apollo/src/main.jsx
+++ apollo/src/main.jsx
@@ -1,10 +1,49 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const client = new ApolloClient({
+  uri: "https://countries.trevorblades.com",
+  cache: new InMemoryCache(),
+});
+
+client
+  .query({
+    query: gql`
+      {
+        country(code: "BR") {
+          name
+          native
+          capital
+          emoji
+          currency
+          languages {
+            code
+            name
+          }
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result));
+const LIST_COUNTRIES = gql`
+  {
+    countries {
+      name
+      code
+    }
+  }
+`;
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
