ea6e40d67de03ef93759754adb1426445c1b0463
vishal-Tx <vishalguptaa2z111@gmail.com>
2023-02-08 02:48:33 +0530

    individual country query started

--- apollo/src/components/Info.jsx
+++ apollo/src/components/Info.jsx
@@ -1,9 +1,29 @@
 import React from "react";
 import { Link, useParams } from "react-router-dom";
+import { gql, useQuery } from "@apollo/client";
 import "../App.css";
+
+const FETCH_COUNTRY_DETAILS = gql`
+  {
+    country(code: ${search}) {
+      name
+      native
+      capital
+      emoji
+      currency
+      languages {
+        code
+        name
+      }
+    }
+  }
+`;
 
 const Info = () => {
   const { search } = useParams();
+  const { data, loading, error } = useQuery(FETCH_COUNTRY_DETAILS);
+  console.log(data);
+
   console.log("search", search);
   return (
     <div className="info">
